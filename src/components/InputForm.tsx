66d34a61d46f77e55fd277eb08e313956ac7aa39
netakiryosuke <sigoryosuke@gmail.com>
2026-01-25 04:50:09 +0900

    Add weight input field to InputForm component

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -4,14 +4,22 @@ import { useState } from "react";
 
 export default function InputForm() {
     const [position, setPosition] = useState<string>("");
+    const [weight, setWeight] = useState<string>("");
 
     return (
-        <div className="flex justify-center">
+        <div className="flex flex-col justify-center">
             <input
                 type="text"
                 value={position}
                 onChange={e => setPosition(e.target.value)}
                 placeholder="属性を入力してください"
+                className="w-auto border rounded text-base focus:outline-none focus:ring-2 focus:ring-blue-500">
+            </input>
+            <input
+                type="text"
+                value={weight}
+                onChange={e => setWeight(e.target.value)}
+                placeholder="支払いの重みを入力してください"
                 className="w-auto border rounded text-base focus:outline-none focus:ring-2 focus:ring-blue-500">
             </input>
         </div>
