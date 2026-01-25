ef660841f793d29478336676133d999e972daa01
netakiryosuke <sigoryosuke@gmail.com>
2026-01-26 00:38:39 +0900

    Remove disabled styles from CalculateButton component

--- src/components/CalculateButton.tsx
+++ src/components/CalculateButton.tsx
@@ -9,8 +9,7 @@ export default function CalculateButton({ onClick }: Props) {
         <button
             type="button"
             className="w-full py-2 bg-blue-600 text-white rounded font-medium
-                    hover:bg-blue-700 transition cursor-pointer
-                    disabled:opacity-60 disabled:cursor-not-allowed"
+                    hover:bg-blue-700 transition cursor-pointer"
             onClick={onClick}
         >
             計算
