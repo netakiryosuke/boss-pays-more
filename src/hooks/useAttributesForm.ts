a2eb266a3fd106e2808da3ed9ff0c1521d118f2f
Ryosuke Osawa <sigoryosuke@gmail.com>
2026-01-25 18:47:00 +0900

    Refactor InputForm to use useAttributesForm hook (#6)
    * Refactor InputForm to use useAttributesForm hook
    
    * Refactor update functions to use a single updateAttribute method

---
+++ src/hooks/useAttributesForm.ts
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { AttributeInput } from "@/types/attribute";
+
+export default function useAttributesForm() {
+    const [attributes, setAttributes] = useState<AttributeInput[]>([
+        { position: "", weight: "", count: "" }
+    ]);
+
+    const updateAttribute = (
+        index: number,
+        field: keyof AttributeInput,
+        value: string
+    ) => {
+        setAttributes(prev =>
+            prev.map((attribute, i) =>
+                i === index
+                    ? { ...attribute, [field]: value }
+                    : attribute
+            )
+        );
+    };
+
+    const updatePosition = (index: number, position: string) => {
+        updateAttribute(index, "position", position);
+    };
+
+    const updateWeight = (index: number, weight: string) => {
+        updateAttribute(index, "weight", weight);
+    };
+
+    const updateCount = (index: number, count: string) => {
+        updateAttribute(index, "count", count);
+    };
+
+    return {
+        attributes,
+        updatePosition,
+        updateWeight,
+        updateCount
+    };
+}
