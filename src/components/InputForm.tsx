875021bd2e8ec94fd1eb5be3c151fb14ac67902a
netakiryosuke <sigoryosuke@gmail.com>
2026-01-25 16:18:17 +0900

    Remove InputFields and integrate AttributeInputGroup

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,33 +1,12 @@
 "use client";
 
-import { useState } from "react";
-import InputField from "./InputField";
+import AttributeInputGroup from "./AttributeInputGroup";
 
 export default function InputForm() {
-    const [position, setPosition] = useState<string>("");
-    const [weight, setWeight] = useState<string>("");
-    const [count, setCount] = useState<string>("");
 
     return (
-        <div className="flex flex-col gap-4">
-            <InputField
-                label="属性"
-                value={position}
-                setValue={setPosition}
-                placeholder="例：部長"
-            />
-            <InputField
-                label="支払いの重み"
-                value={weight}
-                setValue={setWeight}
-                placeholder="例：70"
-            />
-            <InputField
-                label="人数"
-                value={count}
-                setValue={setCount}
-                placeholder="例：3"
-            />
+        <div>
+            <AttributeInputGroup />
         </div>
     );
 }
