d24f9e36227a8da8927747ec06a6983585a94cb9
netakiryosuke <sigoryosuke@gmail.com>
2026-01-26 23:59:11 +0900

    Add ResultItem component to render individual results

---
+++ src/components/ResultItem.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Result } from "@/types/result";
+
+interface Props {
+    result: Result;
+}
+
+export default function ResultItem({ result }: Props) {
+    return (
+        <></>
+    );
+}
